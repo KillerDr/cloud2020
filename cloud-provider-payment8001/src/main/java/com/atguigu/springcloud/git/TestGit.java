0ac8f5f48fc0fb275da5b462052b4894cde84f27
loko <1719698816@qq.com>
2022-06-15 10:53:15 +0800

    second hot-fix commit

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/git/TestGit.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/git/TestGit.java
@@ -3,5 +3,6 @@ package com.atguigu.springcloud.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("this is hot-fix");
+        System.out.println("this is hot-fix");
     }
 }
